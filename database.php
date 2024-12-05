a8cd0f98c36a69db8ba0f73711ac8321804d35c9
JATIN CHULET <2024EB02230@online.bits-pilani.ac.in>
2024-12-05 13:46:27 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostName = "localhost";
-$dbUser = "root";
-$dbPassword = "";
-$dbName = "login_register";
+$hostName = "193.203.184.189";
+$dbUser = "u448196158_wellbe";
+$dbPassword = "@Wellbe.Mindsage2024";
+$dbName = "u448196158_wellbe";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("Something went wrong;");
